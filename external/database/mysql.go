25283af5f8b40adb8b4cda2ebbf1da9b5b8dfa91
kash / Hytus <kash@hytus.net>
2021-03-02 16:12:23 +0900

    Add: mysql connection Add: .env.sample

--- external/database/mysql.go
+++ external/database/mysql.go
@@ -1,11 +1,14 @@
 package database
 
 import (
+	"fmt"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
+	"os"
 )
 
 func newMySQL() gorm.Dialector {
 	// TODO: add mysql driver settings
-	return mysql.Open("")
+	dsn := fmt.Sprintf("%s:%s@tcp(%s)/%s?charset=utf8mb4&parseTime=True&loc=Local", os.Getenv("DB_USER"), os.Getenv("DB_PASSWORD"), os.Getenv("DB_ADDRESS"), os.Getenv("DB_NAME"))
+	return mysql.Open(dsn)
 }
